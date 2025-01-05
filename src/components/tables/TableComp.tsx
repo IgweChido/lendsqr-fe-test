2145235168a923aded0dac225bdd97d9306542a0
Igwe Chidera <igwechiderax@yahoo.com>
2025-01-05 22:44:03 +0100

    (chore):ant table css styles edit

--- src/components/tables/TableComp.tsx
+++ src/components/tables/TableComp.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Table } from "antd";
 import type { TableColumnsType } from "antd";
+import "./tables.scss";
+
+// images
+import dots from "../../assets/icons/three-dots.svg";
+
+// components
 
 interface DataType {
   key: React.Key;
@@ -11,50 +17,59 @@ interface DataType {
 
 const columns: TableColumnsType<DataType> = [
   {
-    title: "Full Name",
+    title: "organization",
     width: 100,
     dataIndex: "name",
     key: "name",
     fixed: "left",
   },
   {
-    title: "Age",
+    title: "Username",
     width: 100,
     dataIndex: "age",
     key: "age",
     fixed: "left",
     sorter: true,
   },
-  { title: "Column 1", dataIndex: "address", key: "1" },
-  { title: "Column 2", dataIndex: "address", key: "2" },
-  { title: "Column 3", dataIndex: "address", key: "3" },
-  { title: "Column 4", dataIndex: "address", key: "4" },
-  { title: "Column 5", dataIndex: "address", key: "5" },
-  { title: "Column 6", dataIndex: "address", key: "6" },
-  { title: "Column 7", dataIndex: "address", key: "7" },
-  { title: "Column 8", dataIndex: "address", key: "8" },
-  { title: "Column 9", dataIndex: "address", key: "9" },
-  { title: "Column 10", dataIndex: "address", key: "10" },
-  { title: "Column 11", dataIndex: "address", key: "11" },
-  { title: "Column 12", dataIndex: "address", key: "12" },
-  { title: "Column 13", dataIndex: "address", key: "13" },
-  { title: "Column 14", dataIndex: "address", key: "14" },
-  { title: "Column 15", dataIndex: "address", key: "15" },
-  { title: "Column 16", dataIndex: "address", key: "16" },
-  { title: "Column 17", dataIndex: "address", key: "17" },
-  { title: "Column 18", dataIndex: "address", key: "18" },
-  { title: "Column 19", dataIndex: "address", key: "19" },
-  { title: "Column 20", dataIndex: "address", key: "20" },
+  { title: "Email", dataIndex: "address", key: "1" },
+  { title: "Phone number", dataIndex: "address", key: "2" },
+  { title: "Date joined", dataIndex: "address", key: "3" },
   {
-    title: "Action",
+    title: "Status",
+    dataIndex: "status",
+    key: "4",
+    render: (status) => (
+      <div className={`status__comp active`}>
+        <p>inactive</p>
+      </div>
+    ),
+  },
+
+  {
+    // title: "Action",
     key: "operation",
     fixed: "right",
-    width: 100,
-    render: () => <a>action</a>,
+    // width: 100,
+    render: () => (
+      <div className="table__action">
+        <img src={dots} alt="" />
+      </div>
+    ),
   },
 ];
 
 const dataSource: DataType[] = [
+  { key: "1", name: "Olivia", age: 32, address: "New York Park" },
+  { key: "2", name: "Ethan", age: 40, address: "London Park" },
+  { key: "1", name: "Olivia", age: 32, address: "New York Park" },
+  { key: "2", name: "Ethan", age: 40, address: "London Park" },
+
+  { key: "1", name: "Olivia", age: 32, address: "New York Park" },
+  { key: "2", name: "Ethan", age: 40, address: "London Park" },
+  { key: "1", name: "Olivia", age: 32, address: "New York Park" },
+  { key: "2", name: "Ethan", age: 40, address: "London Park" },
+  { key: "1", name: "Olivia", age: 32, address: "New York Park" },
+  { key: "2", name: "Ethan", age: 40, address: "London Park" },
   { key: "1", name: "Olivia", age: 32, address: "New York Park" },
   { key: "2", name: "Ethan", age: 40, address: "London Park" },
 ];
@@ -62,8 +77,21 @@ const TableComp = () => {
   return (
     <div>
       <Table<DataType>
-        // className={styles.customTable}
-        pagination={false}
+        pagination={{
+          defaultPageSize: 10,
+          total: 100,
+          showTotal: (total, range) =>
+            `${range[0]}-${range[1]} of ${total} items`,
+          // current: page + 1,
+          showSizeChanger: true,
+          pageSizeOptions: ["10", "15", "20", "30"],
+          // onChange: (page, pageSize) => {
+          //   handlePageChange(page - 1)
+          //   handlePerRowsChange(pageSize, page - 1)
+          //   console.log('page', page - 1)
+          //   console.log('pageSize', pageSize)
+          // },
+        }}
         columns={columns}
         dataSource={dataSource}
         scroll={{ x: "max-content" }}
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,19 +3,48 @@ import "./dashboard.scss";
 
 // images
 import one from "../../assets/icons/analyticsOne.svg";
+import two from "../../assets/icons/analyticsTwo.svg";
+import three from "../../assets/icons/analyticsThree.svg";
+import four from "../../assets/icons/analyticsFour.svg";
 
 // components
 import AnalyticsComp from "../../components/dashboard/AnalyticsComp";
+import TableComp from "../../components/tables/TableComp";
 const Dashboard = () => {
   return (
     <div>
+      {/* header */}
+      <p className="dashboard__p">Users</p>
       {/* dashboard comp */}
-      <AnalyticsComp
-        label="Users"
-        image={one}
-        stats="2,453"
-        color="#DF18FF1A"
-      />
+      <div className="dashboard__analytics">
+        <AnalyticsComp
+          label="Users"
+          image={one}
+          stats="2,453"
+          color="#DF18FF1A"
+        />
+        <AnalyticsComp
+          label="Active Users"
+          image={two}
+          stats="2,453"
+          color="#5718FF1A"
+        />
+        <AnalyticsComp
+          label="Users with Loans"
+          image={three}
+          stats="2,453"
+          color="#F55F441A"
+        />
+        <AnalyticsComp
+          label="Users with Savings"
+          image={four}
+          stats="2,453"
+          color="#FF33661A"
+        />
+      </div>
+
+      {/* table */}
+      <TableComp />
     </div>
   );
 };
